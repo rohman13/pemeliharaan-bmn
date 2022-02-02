e04a9c91622f3beafd158699b4b5ad11d013928d
Beno Pinantau Rohman <rohman13@users.noreply.github.com>
2022-02-02 08:00:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.preprocessing import LabelEncoder, StandardScaler
